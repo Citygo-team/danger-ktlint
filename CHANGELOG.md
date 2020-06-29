6f87c14c49b8855307cce605c57fbc40c2feb7c2
Christopher <saez.chris@gmail.com>
2020-06-29 03:36:10 +0200

    v0.9.0: see changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+## 0.9.0
+### Reloaded
+- Ktlint executable is not mandatory anymore.
+- Limit removed
+- Several options added to be same in mindset than android_lint danger
+
 ## 0.3.0
 ### Added
 - `limit` parameter to set the maximum number of comments of ktlint results.
--- lib/ktlint/plugin.rb
+++ lib/ktlint/plugin.rb
@@ -1,42 +1,116 @@
 require 'json'
 
 module Danger
+  # Kotlin Lint files of a gradle based Android project.
+  # This is done using the Pinterest's [ktlint](https://ktlint.github.io/) tool.
+  # Results are passed out as tables in markdown.
+  #
+  # @example Running AndroidLint with its basic configuration
+  #
+  #          ktlint.lint
+  #
+  # @example Running ktlint with a specific gradle task
+  #
+  #          ktlint.gradle_task = "ktlintMyFlavorVariantCheck"
+  #          ktlint.lint
+  #
+  # @example Running AndroidLint without running a Gradle task
+  #
+  #          ktlint.skip_gradle_task = true
+  #          ktlint.lint
+  #
+  # @see  SaezChristopher/danger-ktlint
+  # @tags ktlint
+  #
+  #
   class DangerKtlint < Plugin
-    
-    class UnexpectedLimitTypeError < StandardError
+
+    # Location of ktlint report file
+    # If your ktlint task outputs to a different location, you can specify it here.
+    # Defaults to "app/build/reports/ktlint/ktlintSourceSetCheck.json". Be carefull only json output is supported for the moment
+    # @return [String]
+    attr_accessor :report_file
+
+    # A getter for `report_file`. You can add multiple files but separeted by commas
+    # @example "file.json,file2.json"
+    # @return [String]
+    def report_file
+      @report_file || 'app/build/reports/ktlint/ktlintSourceSetCheck.json'
     end
 
-    # TODO: Lint all files if `filtering: false`
-    attr_accessor :filtering
+    # Custom gradle task to run.
+    # This is useful when your project has different flavors.
+    # Defaults to "ktlintCheck".
+    # @return [String]
+    attr_accessor :gradle_task
 
-    def limit
-      @limit ||= nil
+    # A getter for `gradle_task`, returning "lint" if value is nil.
+    # @return [String]
+    def gradle_task
+      @gradle_task ||= "ktlintCheck"
     end
 
-    def limit=(limit)
-      if limit != nil && limit.integer?
-        @limit = limit
-      else
-        raise UnexpectedLimitTypeError
-      end
+    # Skip Gradle task.
+    # This is useful when Gradle task has been already executed.
+    # Defaults to `false`.
+    # @return [Boolean]
+    attr_writer :skip_gradle_task
+
+    # A getter for `skip_gradle_task`, returning `false` if value is nil.
+    # @return [Boolean]
+    def skip_gradle_task
+      @skip_gradle_task ||= false
     end
 
-    # Run ktlint task using command line interface
-    # Will fail if `ktlint` is not installed
-    # Skip lint task if files changed are empty
+    # run Ktlint only on stagged file instead of all project file
+    # Defaults to `false`.
+    # @return [Boolean]
+    attr_accessor :use_staged_file_only
+
+    # A getter for `use_staged_file_only`, returning `false` if value is nil.
+    # @return [Boolean]
+    def use_staged_file_only
+      @use_staged_file_only ||= false
+    end
+
+    attr_accessor :treat_errors_as_warnings
+
+    # A getter for `treat_errors_as_warnings`, returning `false` if value is nil.
+    # @return [Boolean]
+    def treat_errors_as_warnings
+      @treat_errors_as_warnings ||= true
+    end
+
+    # Calls lint task of your gradle project.
+    # It fails if `gradlew` cannot be found inside current directory.
+    # It fails if json reports cannot be found.
     # @return [void]
-    # def lint(inline_mode: false)
+    #
     def lint(inline_mode: false)
-      unless ktlint_exists?
-        fail("Couldn't find ktlint command. Install first.")
-        return
+      unless skip_gradle_task
+        return fail("Could not find `gradlew` inside current directory") unless gradlew_exists?
       end
 
-      targets = target_files(git.added_files + git.modified_files)
-      return if targets.empty?
+      unless skip_gradle_task
+        if use_staged_file_only
+          targets = target_files(git.added_files + git.modified_files)
+          system "./gradlew #{gradle_task} -PinternalKtlintGitFilter=\"#{targets.join('\n')}\"" #todo make it work
+        else
+          system "./gradlew #{gradle_task}"
+        end
+      end
 
-      results = JSON.parse(`ktlint #{targets.join(' ')} --reporter=json --relative`)
-      return if results.empty?
+      json_files = report_file.split(',')
+      results = []
+      json_files.each do |jsonFile|
+        unless File.exists?(jsonFile)
+          next
+        end
+        results += JSON.parse(File.read(jsonFile))
+      end
+      if results.empty?
+        print("Skipping ktlinting because no report files available")
+      end
 
       if inline_mode
         send_inline_comments(results)
@@ -45,59 +119,29 @@ module Danger
       end
     end
 
-    # Comment to a PR by ktlint result json
-    #
-    # // Sample ktlint result
-    # [
-    #   {
-    #     "file": "app/src/main/java/com/mataku/Model.kt",
-    # 		"errors": [
-    # 			{
-    # 				"line": 46,
-    # 				"column": 1,
-    # 				"message": "Unexpected blank line(s) before \"}\"",
-    # 				"rule": "no-blank-line-before-rbrace"
-    # 			}
-    # 		]
-    # 	}
-    # ]
+    private
+
     def send_markdown_comment(results)
-      catch(:loop_break) do
-        count = 0
         results.each do |result|
           result['errors'].each do |error|
             file = "#{result['file']}#L#{error['line']}"
             message = "#{github.html_link(file)}: #{error['message']}"
-            fail(message)
-            unless limit.nil?
-              count += 1
-              if count >= limit
-                throw(:loop_break)
-              end
-            end
+            send(treat_errors_as_warnings === true ? "warn" : "fail", message)
+            #fail(message)
           end
         end
-      end
     end
 
     def send_inline_comments(results)
-      catch(:loop_break) do
-        count = 0
         results.each do |result|
           result['errors'].each do |error|
-            file = result['file']
+            #file = result['file']
             message = error['message']
             line = error['line']
-            fail(message, file: result['file'], line: line)
-            unless limit.nil?
-              count += 1
-              if count >= limit
-                throw(:loop_break)
-              end
-            end
+            send(treat_errors_as_warnings === true ? "warn" : "fail", message, file: result['file'], line: line)
+            #fail(message, file: result['file'], line: line)
           end
         end
-      end
     end
 
     def target_files(changed_files)
@@ -106,10 +150,11 @@ module Danger
       end
     end
 
+    def gradlew_exists?
+      `ls gradlew`.strip.empty? == false
+    end
+
     private
 
-    def ktlint_exists?
-      system 'which ktlint > /dev/null 2>&1' 
     end
-  end
 end
